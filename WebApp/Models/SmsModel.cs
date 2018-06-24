32c000f0b132d59893e0794c7509532d92a2fd7e
pkellner <peter@peterkellner.net>
2018-06-24 13:30:47 -0700

    First Checkin, Just model defined

---
+++ WebApp/Models/SmsModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Models
+{
+    public enum SmsDelieveryStatus
+    {
+        New = 1,
+        PendingDeliver = 2,
+        Delivered = 3,
+        Failed = 4
+    }
+
+    public class SmsModel
+    {
+        public string SmsPhoneNumber { get; set; }
+        public string MessageBody { get; set; }
+        public DateTime SmsSentDateTime { get; set; }
+        public string Sid { get; set; }
+        public SmsDelieveryStatus DeliveryStatus { get; set; }
+    }
+}
